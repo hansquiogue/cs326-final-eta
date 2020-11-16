d9c0db6fe25bd081a209d8bb28eff36ec5cf4500
Hans <hansquiogue@gmail.com>
2020-11-15 20:17:40 -0500

    Initial attempt at authentication

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 //--------------------Server--------------------
 
 // Required libraries
-import * as _express from 'express';
+import express from 'express';
 import expressSession from 'express-session';
+import passport from 'passport';
+import passportLocal from 'passport-local';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import * as dotenv from "dotenv";
 
-const express = _express['default'];
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
+dotenv.config({ path: __dirname + '/.env' });
+const LocalStrategy = passportLocal.Strategy;
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -19,14 +22,50 @@ const session = {
     saveUninitialized: false
 };
 
+const strategy = new LocalStrategy(
+    async (username, password, done) => {
+    // No user exists
+    if (!userExists(username)) {
+	    return done(null, false, { 'message' : 'Wrong username' });
+    }
+    // Invalid password
+	if (!validatePass(username, password)) {
+	    // Delay to prevent brute forcing pass attempts
+	    await new Promise((r) => setTimeout(r, 2000));
+	    return done(null, false, { 'message' : 'Wrong password' });
+	}
+	// success!
+	// should create a user object here, associated with a unique identifier
+	return done(null, username);
+});
+
 //--------------------Application--------------------
 
 // App configuration
 app.use(expressSession(session));
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Convert user object to a unique identifier
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+// Convert a unique identifier to a user object
+passport.deserializeUser((uid, done) => {
+    done(null, uid);
+});
 
 // Allows JSON and URLencoded data
 app.use(express.json()); 
 app.use(express.urlencoded({'extended' : true}));
+
+
+//--- Temporary user storage (TODO: Replace/remove with MongoDB)
+// Type: user = { user, *encrypted* pass, email, characters[] }
+const database = [];
+//--------------------------------------------
 
 /*** Application endpoints ***/ 
 
@@ -46,12 +85,11 @@ app.get('/login', (req, res) => {
 // Post request when user attempts to login
 app.post('/login', (req, res) => {
     const user = req.body['username'];
-    const pass = req.body['password'];
-
-    // TODO: Login stuff (Authenticate, etc.)
-    // If login stuff goes okay, redirects to the user's gallery page
-
-    res.redirect('/gallery/user/' + user);
+    // Uses username/password authentication
+    passport.authenticate('local' , {     
+        'successRedirect' : '/gallery/user/' + user,   
+        'failureRedirect' : '/login'      
+    });
 });
 
 // Allows login files to be used
@@ -111,3 +149,72 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
     console.log(`App now listening at port ${port}`);
 });
+
+//--------------------Helper functions--------------------
+
+/**
+ * Checks if user exists
+ * @param {string} username A username
+ * @returns {boolean} Returns true if the username exists in the database
+ */
+function userExists(username) {
+    // Checks users array if username exists
+    return database.filter(user_obj => user_obj.user === username).length > 0;
+}
+
+/**
+ * Validate password (Will need to encrypt later!)
+ * @param {string} username A username
+ * @param {string} password A hashed password (eventually!)
+ * @returns {boolean} Returns true if the password is valid
+ */
+function validatePass(username, password) {
+    const valid = true;
+    // User does not exists
+    if (!userExists(username)) {
+        return !valid;
+    }
+    // Password is incorrect
+    if (database.find(user_obj => user_obj.user === username).password !== password) {
+        return !valid;
+    }
+    return valid;
+}
+
+/**
+ * Adds a user to a database
+ * @param {string} username A username
+ * @param {string} password A hashed password
+ * @param {string} email An email address
+ */
+function addUser(username, password, email) {
+    // User should not exists in database
+    if (userExists(username)) {
+		return false;
+    }
+    // Adds user to data base and returns true
+	database.push({
+        username: username, 
+        password: password,
+        email: email,
+        characters: [] 
+    })
+	return true;
+}
+
+/**
+ * Checks if a user is logged in and redirects them to the
+ * login page if they are not
+ * @param {Object<Request>} req A request made by a client
+ * @param {Object<Response>} res A response to send back to the client
+ * @param {function} next The next route
+ */
+function checkLoggedIn(req, res, next) {
+    // If we are authenticated, we run to the next route
+    if (req.isAuthenticated()) {
+	    next();
+    // Otherwise, redirect to the login page
+    } else {
+	    res.redirect('/login');
+    }
+}
